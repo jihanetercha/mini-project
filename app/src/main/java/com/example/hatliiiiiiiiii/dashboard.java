822beede731315339b214005ca4eea2c3f5d4f3d
mohamedessayd <ballm3957@gmail.com>
2024-05-07 09:22:37 +0100

    new

--- app/src/main/java/com/example/hatliiiiiiiiii/dashboard.java
+++ app/src/main/java/com/example/hatliiiiiiiiii/dashboard.java
@@ -42,6 +42,7 @@ public class dashboard extends AppCompatActivity {
         });
 
 
+        //new comment
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
